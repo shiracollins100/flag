84c014eab46b52740ee22eaac70838210f14963f
jbt <shiracollins100@gmail.com>
2026-08-16 11:28:09 +0300

    all modules

--- screen.py
+++ screen.py
@@ -1,4 +1,26 @@
 import pygame
+from sys import exit # to close the game
+import consts
+
 
 pygame.init()
-screen = pygame.display.set_mode((800, 600))
+screen = pygame.display.set_mode((consts.SCREEN_WIDTH, consts.SCREEN_HEIGHT))
+pygame.display.set_caption("Flag Game")
+clock = pygame.time.Clock()
+
+while True: # the game loop, when true game is running
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+
+    pygame.display.update()
+    clock.tick(60) # updates the changes on screen - moving soldier
+
+
+
+
+
+
+
+
