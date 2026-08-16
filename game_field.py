f94c4979d2bf5f3502024909559cc4dc4a448716
jbt <shiracollins100@gmail.com>
2026-08-16 14:13:32 +0300

    all modules

--- game_field.py
+++ game_field.py
@@ -1,8 +1,28 @@
+import consts
+from random import randint
 
 # the matrix of the game
-def create_field_matrix():
-    field_matrix = [[[] for x in range(50)] for y in range(25)]
-    return field_matrix
+
+# create grass screen
+def create_grass_field_matrix():
+    grass_field_matrix = [[[] for x in range(consts.COL)] for y in range(consts.ROW)]
+    for row in range(consts.ROW):
+        for col in range(consts.COL):
+            pos = randint(0, consts.EMPTY_COLS )
+            if pos == 0:
+                grass_field_matrix[row][col] = consts.GRASS_COLS
+    return grass_field_matrix
+
+
+# create xray screen
+def create_gray_field_matrix():
+    xray_field_matrix = [[[] for x in range(consts.COL)] for y in range(consts.ROW)]
+    for row in range(consts.ROW):
+        for col in range(consts.COL):
+            pos = randint(0, consts.EMPTY_COLS )
+            if pos == 0:
+                xray_field_matrix[row][col] = consts.BOMB_COLS
+    return xray_field_matrix
 
 def get_x_y_position(row, column):
     pass
--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 import pygame
+import consts
+import screen
+import game_field
+import soldier
 
+state = { "is_playing" : True,
+        "screen": None,
+        "field" : None,
+        "land_on_mind" : None
 
+}
 
 
 
